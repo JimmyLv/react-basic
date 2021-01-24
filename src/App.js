6fd79139fa0b05c445621abef5a11ec23166fbaa
JimmyLv <jimmy.jinglv@gmail.com>
2021-01-24 16:34:35 +0800

    polished

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import { Layout } from "antd";
-import React from "react";
-import "./App.css";
-import TodoList from "./components/TodoList";
-import { useLocalStorageValue } from "./hooks/useLocalStorageValue";
-import logo from "./logo.svg";
-import TaskContext, { TaskProvider } from "./hooks/useTodo";
+import { Layout } from 'antd'
+import React from 'react'
+import './App.css'
+import TodoList from './components/TodoList'
+import { TaskProvider } from './hooks/useTodo'
+import logo from './logo.svg'
 
-const { Header, Content, Sider } = Layout;
+const { Header, Content, Footer } = Layout;
 
 function App() {
-  const [name, setName] = useLocalStorageValue("name", "React");
-  const [age, setAge] = useLocalStorageValue("age", 0);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    alert(`hello ${name}!`);
-  }
-
   return (
     <div className="App">
       <Header theme="light">
@@ -28,7 +19,7 @@ function App() {
         </TaskProvider>
         <hr />
       </Content>
-      <Sider>Sider</Sider>
+      <Footer>Footer</Footer>
     </div>
   );
 }
